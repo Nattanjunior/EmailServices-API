95c6c42c64c539d324955cab736f7f7b25cd23f1
nattanjunior <fnatanieljunior@gmail.com>
2024-09-20 20:06:31 -0300

    Create: UseCases, create class EmailSenderUsecase

--- src/core/EmailSenderUseCase.ts
+++ src/core/EmailSenderUseCase.ts
@@ -1,11 +1,13 @@
 // casos de uso, contrato
 
-interface EmailProps {
+export interface EmailProps {
   to: string,
   subject: string,
   body: string
 }
 
 class EmailSenderUseCase {
-  async sendEmail({ to, subject, body }: EmailProps) { }
+  async sendEmail({ to, subject, body }: EmailProps): Promise<void> { }
 }
+
+export { EmailSenderUseCase }
